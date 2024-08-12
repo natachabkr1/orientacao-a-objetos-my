96b59c1d511b2dff0650cba2fc7ed60168c0ff72
natachabkr1 <natachabkr@hotmail.com>
2024-08-12 18:55:15 -0300

    primeiros arquivos

---
+++ funcoes_do_log.py
@@ -0,0 +1,14 @@
+# Dentro do espaço de nomes do meu mkódulo - funçõs do log, eu defini uma variável e uma função
+
+nome_do_usuario = 'Dori'
+
+def imprimir_no_log(texto, nivel='info'):
+    if nivel == 'info':
+        print(f'INFO: {texto}')
+    elif nivel == 'alerta':
+        print(f'ALERTA: {texto}')
+    elif nivel == 'erro':
+        print(f'ERRO: {texto}')
+    else:
+        print(f'[ERRO] Nível "{nivel}" não é válido!')
+        
---
+++ import.py
@@ -0,0 +1,5 @@
+# Vamos importar o módulo que criamos - funções do log (estão na mesma pasta)
+
+import funcoes_do_log
+
+funcoes_do_log.imprimir_no_log('Olá mundo!')
